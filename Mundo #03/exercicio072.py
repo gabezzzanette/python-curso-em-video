43bada1ecd8c794e469a6b4e7ecade5654e63d3d
Gabe <gabe-z@hotmail.com>
2021-09-04 23:36:33 -0300

    exercicio073 feito e 072 melhorado

--- Mundo #03/exercicio072.py
+++ Mundo #03/exercicio072.py
@@ -1,21 +1,27 @@
-
-#
 # Exercício Python 72: Crie um programa que tenha uma tupla totalmente
 # preenchida com uma contagem por extenso, de zero até vinte.
 # Seu programa deverá ler um número pelo teclado (entre 0 e 20)
 # e mostrá-lo por extenso
 #
 
-tupla = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
-         'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito',
-         'dezenove', 'vinte')
-
+tupla = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco',
+         'seis', 'sete', 'oito', 'nove', 'dez', 'onze',
+         'doze', 'treze', 'quatorze', 'quinze', 'dezesseis',
+         'dezessete', 'dezoito', 'dezenove', 'vinte')
 
 while True:
-    print('---' * 4)
-    num = int(input('Digite um numero entre ZERO E VINTE \n'))
-
-    if num < 0 or num > 20:
-        print('Número inválido, tente novamente')
-    else:
-        print(f'Digitado numero [{num}] corresponde ao [{tupla[num]}]\n')
+    while True:
+        print('---' * 4)
+        num = int(input('Digite um numero entre ZERO E VINTE \n'))
+        
+        if num < 0 or num > 20:
+            print('Número inválido!')
+        else:
+            print(f'Número {tupla[num]}\n')
+        break
+    rSair = ' '
+    if rSair not in 'SN':
+        rSair = str(input('Deseja digitar novamente? S/N')).strip().upper()
+    if rSair == 'N':
+        print('[....Finalizando....]')
+        break
