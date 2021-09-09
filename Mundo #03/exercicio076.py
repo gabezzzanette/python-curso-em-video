809b85f7ba3b7dfa353346ff51d254b69ffa8008
Gabe <gabe-z@hotmail.com>
2021-09-08 21:48:24 -0300

    primeiro

---
+++ Mundo #03/exercicio076.py
@@ -0,0 +1,17 @@
+# Exercício Python 076: Crie um programa que tenha uma
+# tupla única com nomes de produtos e seus respectivos preços,
+# na sequência. No final, mostre uma listagem de preços,
+# organizando os dados em forma tabular.
+
+
+tabela = ('Sofá', 950.99,
+          'Cadeira de Praia', 74.49,
+          'Máquina de Lavar Roupa', 1299.00,
+          'Tv 50 polegadas', 2799.99,
+          'Notebook', 3500.49)
+
+for t in range(0, len(tabela)):
+    if t % 2 == 0:
+        print(f'{tabela[t]:.<33}', end='')
+    else:
+        print(f'R${tabela[t]:>10}')
