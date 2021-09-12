31299076ff1352b8a46125a3a07eed19528189e3
Gabe <gabe-z@hotmail.com>
2021-09-12 13:58:25 -0300

    first commit

---
+++ Mundo #03/exercicio077.py
@@ -0,0 +1,14 @@
+# Exercício Python 077: Crie um programa que tenha uma tupla
+# com várias palavras (não usar acentos). Depois disso, você
+# deve mostrar, para cada palavra, quais são as suas vogais.
+
+palavras = ('Cafe', 'Dormir', 'Cachorro', 'Arvore',
+            'Montanha', 'Dinheiro', 'Presidente', 'Genocida')
+
+# achar as letras vogais da palavra
+for palavra in palavras:
+    print(f'\nNa palavra {palavra} temos - ', end=' ')
+    for letra in palavra:
+        
+        if letra.lower() in 'aeio':
+            print(letra.upper(), end=' ')
