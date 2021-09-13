a0db630667196076384e5c5e72155ad712ce4b37
Gabe <gabe-z@hotmail.com>
2021-09-13 08:03:32 -0300

    troca de lista para tupla e melhoria na variavel de saída

--- Mundo #03/exercicio072.py
+++ Mundo #03/exercicio072.py
@@ -4,16 +4,15 @@
 # e mostrá-lo por extenso
 #
 
-tupla = ['zero', 'um', 'dois', 'tres', 'quatro', 'cinco',
+tupla = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco',
          'seis', 'sete', 'oito', 'nove', 'dez', 'onze',
          'doze', 'treze', 'quatorze', 'quinze', 'dezesseis',
-         'dezessete', 'dezoito', 'dezenove', 'vinte']
+         'dezessete', 'dezoito', 'dezenove', 'vinte')
 
 while True:
     while True:
         print('---' * 4)
         num = int(input('Digite um numero entre ZERO E VINTE \n'))
-        
         if num < 0 or num > 20:
             print('Número inválido!')
         else:
@@ -21,7 +20,7 @@ while True:
         break
     rSair = ' '
     if rSair not in 'SN':
-        rSair = str(input('Deseja digitar novamente? S/N'))
+        rSair = str(input('Deseja digitar novamente? S/N')).upper().strip()[:1]
     if rSair == 'N':
         print('[....Finalizando....]')
         break
